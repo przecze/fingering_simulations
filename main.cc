12da5385dba9e8b0e9428b16c1b2df8d6d460190
Przemyslaw Czechowski <przemyslaw.czechowski@gmail.com>
2017-08-16 22:04:09 +0200

    run 017 maybe okay

--- main.cc
+++ main.cc
@@ -17,7 +17,7 @@ void SingleManagerTest() {
   std::cout<<"open: "<<out_filename<<std::endl;
   std::ofstream analyser_out(out_filename);
 
-  SingleManager simple(data_out, analyser_out, 0.5);
+  SingleManager simple(data_out, analyser_out, 3);
   simple.Init();
   simple.Run();
   
